645ff503febd462abe61a2d10ada1bf314beb2ac
anushkashah <44152015+anushkashah@users.noreply.github.com>
2020-03-31 15:25:09 -0700

    Added Form
    Added form

---
+++ form-to-email.php
@@ -0,0 +1,33 @@
+<?php
+if(!isset($_POST['submit']))
+{
+	//This page should not be accessed directly. Need to submit the form
+	echo "error; you need to submit the form!";
+}
+$name = $_POST['contactname']
+$visitor_email = $_POST['contactemail']
+$subject = $_POST['contactsubject']
+$message = $_POST['contactmessage']
+
+//Validate first
+if(empty($name) || empty($visitor_email))
+{
+	echo "Name and email are mandatory!";
+	exit;
+}
+
+$email_from = 'anushkashah654@gmail.com';
+$email_subject = "New Form Submission";
+$email_body = "You have recieved a new message from the user $name. \n".
+	"email address: $visitor_email \n".
+	"Here is the subject: $subject \n".
+	"Here is the message: $message".
+
+$to = "anushkashah654@gmail.com";
+$headers = "From: $email_from \r\n";
+
+//Send the email
+mail($to, $email_subject, $email_body, $headers);
+
+?>
+
