7402fbce9d87e97a1865a6e94c33753e629296e3
Isis Decrem <60768505+isisdecrem@users.noreply.github.com>
2020-03-31 17:01:19 -0700

    try again

--- form-to-email.php
+++ form-to-email.php
@@ -1,8 +1,8 @@
 <?php
 if(!isset($_POST['submit']))
 {
-	//This page should not be accessed directly. Need to submit the form
-	echo "error; you need to submit the form!";
+    //This page should not be accessed directly. Need to submit the form
+    echo "error; you need to submit the form!";
 }
 $name = $_POST['contactname']
 $visitor_email = $_POST['contactemail']
@@ -12,16 +12,16 @@ $message = $_POST['contactmessage']
 //Validate first
 if(empty($name) || empty($visitor_email))
 {
-	echo "Name and email are mandatory!";
-	exit;
+    echo "Name and email are mandatory!";
+    exit;
 }
 
 $email_from = 'isisdecrem@gmail.com';
 $email_subject = "New Form Submission";
 $email_body = "You have recieved a new message from the user $name. \n".
-	"email address: $visitor_email\n".
-	"Here is the subject: $subject\n".
-	"Here is the message: \n $message".
+    "email address: $visitor_email \n".
+    "Here is the subject: $subject \n".
+    "Here is the message: $message".
 
 $to = "isisdecrem@gmail.com";
 $headers = "From: $email_from \r\n";
